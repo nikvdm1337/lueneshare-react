71fe8a13ec184467d22f2186ab3f943872228cfa
NikolaCirisanovic <nikola.cirisanovic@outlook.de>
2019-07-22 15:08:53 +0700

    too lazy to read into react hooks. doing it oldschool for now

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import NavbarMain from './NavMain';
 import SidebarMain from './Sidebar';
@@ -7,19 +7,37 @@ import {Container, Row, Col} from 'reactstrap';
 
 
 
+class App extends Component {
+	// Data
+	constructor() {
+		super();
+		this.state = {
+			category:'',
+		};
+	}
 
-function App() {
-  return (
+	// Functions
+	setCategory = (id) => {
+		this.setState({
+			category: id
+		})
+		console.log('pressed link')
+	}
+	// Render
+	render() {
+		return (
+
     <div className="megawrap">
      <NavbarMain sticky={'top'}/>
      <Container>
       <Row>
-        <Col sm={2}> <SidebarMain /> </Col>
+        <Col sm={2}> <SidebarMain setCategory={this.setCategory} /> </Col>
         <Col sm={10}> <ProductsMain /> </Col> 
       </Row>
     </Container>
     </div>
   );
+}
 }
 
 export default App;
