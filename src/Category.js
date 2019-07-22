eb92d7aeff2adcde9671a87600e9a9b4d19b324e
NikolaCirisanovic <nikola.cirisanovic@outlook.de>
2019-07-22 15:09:06 +0700

    added category component

---
+++ src/Category.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react'
+import { Nav } from 'react-bootstrap';
+
+class Category extends Component {
+	// Data
+	
+	// Functions
+	componentWillMount() {
+		console.log(this.props)
+	}
+
+	// Render
+	render() {
+		return (
+			<Nav.Link onClick={() => this.props.setCategory(this.props.categoryID)}>{this.props.category.name}</Nav.Link>
+		)
+	}
+}
+
+export default Category
