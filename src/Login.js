e1ba043b4f56f074cf2bff1c8dbeaab93233ff76
NikolaCirisanovic <nikola.cirisanovic@outlook.de>
2019-07-22 15:07:22 +0700

    auth on login

--- src/Login.js
+++ src/Login.js
@@ -1,22 +1,58 @@
 import React from 'react';
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import axios from 'axios';
 import './Login.css'
 
 export default class Login extends React.Component {
-  //Function
-  submitlogin = (e) => {
-    e.preventDefault()
+  // Data
+  constructor() {
+		super();
+	this.state = {
+		email: '',
+		password: '',
+		error: ''
   }
+}
+	// Functions
+	
+
+	changeEmail = (e) => {
+		this.setState({email: e.target.value})
+	}
+
+	changePassword = (e) => {
+		this.setState({password: e.target.value})
+	}
+
+	login = (e) => {
+		e.preventDefault()
+		axios.post(`http://localhost:2000/api/login`, this.state).then((res) => {
+			if (!res.data.token) {
+				this.setState({
+					error: res.data
+				})
+			} else {
+				this.setState({
+					error: ''
+				})
+				localStorage.setItem('token', res.data.token)
+				this.props.auth()
+			}
+		}).catch((err) => {
+			console.log('err!!!!', err)
+		})
+	}
+
   render() {
     return (
-      <Form inline onSubmit={(e)=> this.submitlogin(e)}>
+      <Form inline onSubmit={(e) => this.login(e)}>
         <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
           <Label for="exampleEmail" className="mr-sm-2">Email</Label>
-          <Input type="email" name="email" id="exampleEmail" placeholder="something@idk.cool" />
+          <Input type="email" name="email" id="exampleEmail" placeholder="something@idk.cool" value={this.state.email} onChange={(e) => this.changeEmail(e)} />
         </FormGroup>
         <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
           <Label for="examplePassword" className="mr-sm-2">Password</Label>
-          <Input type="password" name="password" id="examplePassword" placeholder="don't tell!" />
+          <Input type="password" name="password" id="examplePassword" placeholder="don't tell!" value={this.state.password} onChange={(e) => this.changePassword(e)} />
         </FormGroup>
         <Button>Submit</Button>
       </Form>
--- src/Routes.js
+++ src/Routes.js
@@ -3,17 +3,32 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import Login from './Login'
 import App from './App'
 import Signup from './Signup'
+import Submit from './Submit'
 
 class Routes extends Component {
 	// Data
-
+	state = {}
+	// Functions
+	checkAuth = () => {
+		if (localStorage.getItem('token')) {
+			return true
+		} else {
+			return false
+		}
+	}
+	auth = () => {
+		if (this.checkAuth()) {
+			window.location.href = '/'
+		}
+	}
 	// Render
 	render() {
 		return (
 			<BrowserRouter>
 				<Switch>
-					<Route path="/login" component={() => <Login />} />
-					<Route path="/signup" component ={() => <Signup />} />
+					<Route path="/login" component={() => <Login auth={this.auth} />} />
+					<Route path="/signup" component ={() => <Signup auth={this.auth} />} />
+					<Route path="/submit" component = {() => <Submit />} />
                     <Route path="/" component={() => <App />} />
 					
 				</Switch>
