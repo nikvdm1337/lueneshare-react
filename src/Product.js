39bae0c7d33a569dad8a35f2805d38049a6d7ed7
NikolaCirisanovic <nikola.cirisanovic@outlook.de>
2019-07-28 16:46:43 +0700

    rendering products from db

--- src/Product.js
+++ src/Product.js
@@ -3,22 +3,28 @@ import { Card, Button, CardImg, CardTitle, CardText, CardGroup,
  CardSubtitle, CardBody } from 'reactstrap';
 import './Product.css'
 import {Col} from 'reactstrap'; 
+import moment from 'moment'
 
 
 
 export default class Product extends React.Component {
+ 
+  
+componentDidMount() {
+  console.log(this.props.product)
+}
   render() {
     return (
           
           <Col sm={6} className="product">
             <CardGroup>
               <Card>
-                <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=256&h=180" alt="Card image cap" />
+                <CardImg top width="100%" src={this.props.product.image} alt="Product image" />
                 <CardBody>
-                  <CardTitle>Card title</CardTitle>
-                  <CardSubtitle>Card subtitle</CardSubtitle>
-                  <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
-                  <Button>Button</Button>
+                  <CardTitle>{this.props.product.title} @ {this.props.product.category}</CardTitle>
+                  <CardSubtitle>von {this.props.product.author.name}  gepostet um   {moment(this.props.product.date).format('D MMM YY hh:m A')}</CardSubtitle>
+                  <CardText>{this.props.product.description}</CardText>
+                  <Button>Zum Produkt</Button>
                 </CardBody>
               </Card>
               </CardGroup>
--- src/ProductsMain.js
+++ src/ProductsMain.js
@@ -1,20 +1,35 @@
 import React from 'react';
 import Product from './Product'
 import './Product.css'
+import axios from 'axios'
 
 export default class ProductsMain extends React.Component {
+  // Data
+	constructor() {
+		super();
+		this.state = {
+			products:[],
+		};
+  }
+  componentDidMount() {
+		axios.get(`http://localhost:2000/api/products`).then((res) => {
+			console.log(res.data)
+			this.setState({
+				products: res.data
+			})	
+			
+		}).catch((err) => {
+			console.log('err', err)
+		})
+  }
   render() {
     return (
       <div className="productsmain row">
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
+					{
+						this.state.products.map((m) => {
+							return <Product product={m} key={m._id} />
+						})
+					}
       </div>
     )
   }
