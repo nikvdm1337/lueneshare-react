817f49c2170cc16eae5fcd8d282af5777bd4792f
NikolaCirisanovic <nikola.cirisanovic@outlook.de>
2019-07-29 17:13:58 +0700

    empty component

---
+++ src/MyProfile.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class MyProfile extends Component {
+    
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default MyProfile;
