5c7cdb2e451cb56e5b21056de2df2f00e66666ec
NikolaCirisanovic <nikola.cirisanovic@outlook.de>
2019-07-29 17:12:40 +0700

    added links to social media icons

--- src/Footer.js
+++ src/Footer.js
@@ -8,10 +8,9 @@ export default class Footer extends React.Component {
         <div className="footer">
         <div className="container">
                <span> 
-                <a href='www.google.de'><i className="fa fa-github"></i></a>
-                <a href='www.google.de'><i className="fa fa-twitter fa-3x fa-fw"></i></a>
-                <a href='www.google.de'><i className="fa fa-youtube-play fa-3x fa-fw"></i></a>
-                <a href='www.google.de'><i className="fa fa-linkedin fa-3x fa-fw"></i></a>
+                <a href='https://github.com/NikolaCirisanovic' rel="noopener noreferrer" target="_blank"><i className="fa fa-github"></i></a>
+                <a href='https://twitter.com/nikolacirisano1' rel="noopener noreferrer" target="_blank"><i className="fa fa-twitter fa-3x fa-fw"></i></a>
+                <a href='https://www.linkedin.com/in/nikola-cirisanovic-69116a185/' rel="noopener noreferrer" target="_blank"><i className="fa fa-linkedin fa-3x fa-fw"></i></a>
               </span>
         </div>
       </div>
