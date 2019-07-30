78c1e9e38d95752f32dc668bfc7272c044aa41fa
NikolaCirisanovic <nikola.cirisanovic@outlook.de>
2019-07-31 02:39:54 +0700

    profile component

--- src/Profile.js
+++ src/Profile.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import axios from 'axios'
 import NavMain from './NavMain'
+import { Col, Row } from 'reactstrap';
+
+import './Profile.css'
 
 class Profile extends Component {
     state = {
         user: {}
     }
+    
 
     getProfile = () => {
 		axios.get('http://localhost:2000/api/profile', 
@@ -17,7 +21,7 @@ class Profile extends Component {
 			this.setState({
                 user: res.data
 			}, () => {
-                console.log('profilestate', this.state)
+                console.log('profilestate', this.state.user.profilepic)
             })
 		}).catch((err) => {
 			console.log('err', err)
@@ -27,15 +31,31 @@ class Profile extends Component {
     componentWillMount() {
         this.getProfile()
     }
-
     render() {
+        
+
         return (
-            <div>
+            <div className="entireProfilePage">
             <NavMain />
-                <ul>
-                    <li>{this.state.user.name}</li>
-                    <li>{this.state.user.email}</li>
-                </ul>
+            <div className="profileContainer">
+                <Row>
+                    <Col xs="4" className="ProfilePicture">
+                    <h3>Mein Profilbild</h3>
+                        <div className="profpic" 
+                        style={{ backgroundImage: `url(${this.state.user.profilepic})` }}>
+                        </div>
+                    </Col>
+                    <Col xs="4" className="ProfileContent">
+                        <h2>Name:</h2>
+                        <p>{this.state.user.name}</p>
+                        <h2>E-Mail:</h2>
+                        <p>{this.state.user.email}</p>
+                        <h2>City:</h2>
+                        <p>{this.state.user.city}</p>
+                        
+                    </Col>
+                </Row>
+            </div>
             </div>
         );
     }
