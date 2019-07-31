f2aaa6ebee052d0bd3abaa36513606d75f1b3ef2
NikolaCirisanovic <nikola.cirisanovic@outlook.de>
2019-07-31 13:27:38 +0700

    imported moment to format time

--- src/Message.js
+++ src/Message.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Card, CardTitle, CardText, CardGroup, CardBody } from 'reactstrap';
+import moment from 'moment'
 
 class Message extends Component {
     render() {
@@ -7,7 +8,7 @@ class Message extends Component {
             <CardGroup>
             <Card>
               <CardBody sm={2}>
-                <CardTitle className="userName">{this.props.message.author} <span className="date"> {this.props.message.date} </span> </CardTitle>  
+                <CardTitle className="userName">{this.props.message.author.name} <span className="date"> {moment(this.props.message.date).format('D MMM YY hh:m A')} </span> </CardTitle>  
                 <CardText className="textBody">{this.props.message.body}</CardText>
               </CardBody>
             </Card>
