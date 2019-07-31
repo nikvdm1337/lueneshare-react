a6c67e889f198f7cc3ce58e461277b0fe0ce9152
NikolaCirisanovic <nikola.cirisanovic@outlook.de>
2019-07-31 17:50:07 +0700

    classname to "back" button

--- src/FullScreenProduct.js
+++ src/FullScreenProduct.js
@@ -8,19 +8,20 @@ import CommentSection from './CommentSection'
 export const FullScreenProduct = (props) => {
   console.log(props.messages)
   console.log(props.product._id)
-
+  
     return (
         <Row className="productsingle">
         <Col sm={5}>
         <CardGroup>
           <Card>
+          <Button className="onFullScreenButton">Schreibe mir</Button>
             <CardBody sm={2}>
             <CardImg sm={2}top width="100%" src={props.product.file} alt="Product image" />
               <CardTitle className="producttitle">{props.product.title} - <span className="productcategory">{props.product.category.name}</span></CardTitle>
               <CardSubtitle>von <span className="productauthor"> {props.product.author.name}</span> gepostet um <span className="date"> {moment(props.product.date).format('D MMM YY hh:m A')} </span></CardSubtitle>
               <div className="description">Beschreibung:</div>
               <CardText>{props.product.description}</CardText>
-              <Button onClick={() => props.showAllProducts()}>Zurück</Button>
+              <Button className="onFullScreenButton" onClick={() => props.showAllProducts()}>Zurück</Button>
             </CardBody>
           </Card>
           </CardGroup>
