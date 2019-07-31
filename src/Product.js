c87e050f55d6643654b0a91aab2fccd12acc89d6
NikolaCirisanovic <nikola.cirisanovic@outlook.de>
2019-07-31 17:51:04 +0700

    Clicking on entire card mounts the FullScreenProduct now

--- src/Product.js
+++ src/Product.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Button, CardImg, CardTitle, CardText, CardGroup,
+import { Card, Button, CardTitle, CardText, CardGroup,
  CardSubtitle, CardBody } from 'reactstrap';
 import './Product.css'
 import {Col} from 'reactstrap'; 
@@ -10,18 +10,18 @@ import moment from 'moment'
 export default class Product extends React.Component {
  
   render() {
+
     return (
-  
-          <Col sm={6} className="product">
-            <CardGroup>
-              <Card>
-                <CardImg top width="100%" src={this.props.product.file} alt="Product image" />
+          <Col xs={6}>
+            <CardGroup onClick={ () => this.props.selectProductForFullscreenView(this.props.product._id)}>
+              <Card className="product">
+                <div className="TopImage" top width="100%" style={{ backgroundImage: `url(${this.props.product.file})` }}/>
                 <CardBody>
                   <CardTitle className="producttitle">{this.props.product.title} - <span className="productcategory">{this.props.product.category.name}</span></CardTitle>
                   <CardSubtitle>von <span className="productauthor"> {this.props.product.author.name}</span> gepostet um <span className="date"> {moment(this.props.product.date).format('D MMM YY hh:mm A')} </span></CardSubtitle>
                   <div className="description">Beschreibung:</div>
                   <CardText>{this.props.product.description}</CardText>
-                  <Button onClick={ () => this.props.selectProductForFullscreenView(this.props.product._id) } className="goToProduct">Zum Produkt</Button>
+                  <Button className="goToProduct">Zum Produkt</Button>
                 </CardBody>
               </Card>
               </CardGroup>
