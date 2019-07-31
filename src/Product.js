a0e65c1980b4c031f166ab4c925d3a169eb91c6f
NikolaCirisanovic <nikola.cirisanovic@outlook.de>
2019-07-31 13:27:13 +0700

    adapting to changed state name

--- src/Product.js
+++ src/Product.js
@@ -15,7 +15,7 @@ export default class Product extends React.Component {
           <Col sm={6} className="product">
             <CardGroup>
               <Card>
-                <CardImg top width="100%" src={this.props.product.image} alt="Product image" />
+                <CardImg top width="100%" src={this.props.product.file} alt="Product image" />
                 <CardBody>
                   <CardTitle className="producttitle">{this.props.product.title} - <span className="productcategory">{this.props.product.category.name}</span></CardTitle>
                   <CardSubtitle>von <span className="productauthor"> {this.props.product.author.name}</span> gepostet um <span className="date"> {moment(this.props.product.date).format('D MMM YY hh:mm A')} </span></CardSubtitle>
