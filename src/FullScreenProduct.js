dd034ea59373296114a3d177a606b78aaf75dae2
NikolaCirisanovic <nikola.cirisanovic@outlook.de>
2019-08-01 16:16:36 +0700

    refactoring code to classes.
    functional components are pain when you need lifecycles

--- src/FullScreenProduct.js
+++ src/FullScreenProduct.js
@@ -4,30 +4,54 @@ import { Card, Button, CardImg, CardTitle, CardText, CardGroup,
 import moment from 'moment'
 import './FullScreenProduct.css'
 import CommentSection from './CommentSection'
+import axios from 'axios'
 
-export const FullScreenProduct = (props) => {
-  console.log(props.messages)
-  console.log(props.product._id)
+class FullScreenProduct extends React.Component {
+
+  state = {
+    messages: []
+  }
+
+  getAllMessages = () => {
+		axios.get(`http://localhost:2000/api/messages?product=${this.props.product._id}`).then((res) => {
+			console.log('messages', res.data)
+			this.setState({
+				messages: res.data
+			});
+			
+		}).catch((err) => {
+			console.log('err', err)
+		})
+  }
   
+  componentDidMount() {
+    this.getAllMessages()
+  }
+  
+  
+
+  render() {
     return (
-        <Row className="productsingle">
+      <Row className="productsingle">
         <Col sm={5}>
         <CardGroup>
           <Card>
           <Button className="onFullScreenButton">Schreibe mir</Button>
             <CardBody sm={2}>
-            <CardImg sm={2}top width="100%" src={props.product.file} alt="Product image" />
-              <CardTitle className="producttitle">{props.product.title} - <span className="productcategory">{props.product.category.name}</span></CardTitle>
-              <CardSubtitle>von <span className="productauthor"> {props.product.author.name}</span> gepostet um <span className="date"> {moment(props.product.date).format('D MMM YY hh:m A')} </span></CardSubtitle>
+            <CardImg sm={2}top width="100%" src={this.props.product.file} alt="Product image" />
+              <CardTitle className="producttitle">{this.props.product.title} - <span className="productcategory">{this.props.product.category.name}</span></CardTitle>
+              <CardSubtitle>von <span className="productauthor"> {this.props.product.author.name}</span> gepostet um <span className="date"> {moment(this.props.product.date).format('D MMM YY hh:m A')} </span></CardSubtitle>
               <div className="description">Beschreibung:</div>
-              <CardText>{props.product.description}</CardText>
-              <Button className="onFullScreenButton" onClick={() => props.showAllProducts()}>Zurück</Button>
+              <CardText>{this.props.product.description}</CardText>
+              <Button className="onFullScreenButton" onClick={() => this.props.showAllProducts()}>Zurück</Button>
             </CardBody>
           </Card>
           </CardGroup>
           </Col>
-          <CommentSection messages={props.messages} createMessage={props.createMessage}/>
+          <CommentSection messages={this.state.messages} createMessage={this.props.createMessage}/>
           </Row>
-    );
-};
-
+    )
+  }
+}
+        
+export default FullScreenProduct
