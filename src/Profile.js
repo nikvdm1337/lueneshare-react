42c2c9a89af9beef56f95d933a3e393ed0d4bac8
NikolaCirisanovic <nikola.cirisanovic@outlook.de>
2019-08-02 16:06:15 +0700

    rendering products for each profile

--- src/Profile.js
+++ src/Profile.js
@@ -2,31 +2,51 @@ import React, { Component } from 'react';
 import axios from 'axios'
 import NavMain from './NavMain'
 import { Col, Row } from 'reactstrap';
+import MyProduct from './MyProduct'
 
 import './Profile.css'
 
 class Profile extends Component {
     state = {
-        user: {}
+        user: {},
+        products:[],
+        selectedProduct:''
     }
     
-
+    getProductsForMyProfile = () => { 
+        axios.get(`${process.env.REACT_APP_API}/api/products?author=${this.state.user._id}`, 
+        {headers: {
+			Authorization: `Bearer ${localStorage.getItem('token')}`
+		}}
+        ).then((res) => {
+            this.setState({
+                products: res.data
+            }, () => console.log('products', this.state.products))
+        }).catch((err) => {
+            console.log('err', err)
+        })
+    }
     getProfile = () => {
 		axios.get(`${process.env.REACT_APP_API}/api/profile`, 
 		{headers: {
 			Authorization: `Bearer ${localStorage.getItem('token')}`
 		}}
 		).then((res) => {
-			console.log('profile',res.data)
 			this.setState({
                 user: res.data
 			}, () => {
-                console.log('profilestate', this.state.user.profilepic)
+                this.getProductsForMyProfile()
             })
 		}).catch((err) => {
 			console.log('err', err)
 		})
     }
+
+    setProductID = (id) => {
+		this.setState({
+			selectedProduct: id
+		}, () => console.log(id))
+	}
 
     componentWillMount() {
         this.getProfile()
@@ -39,20 +59,28 @@ class Profile extends Component {
             <NavMain />
             <div className="profileContainer">
                 <Row>
-                    <Col xs="4" className="ProfilePicture">
-                    <h3>Mein Profilbild</h3>
+                    <Col sm="2" className="ProfilePicture">
+                    <h3>Mein Profil</h3>
                         <div className="profpic" 
                         style={{ backgroundImage: `url(${this.state.user.profilepic})` }}>
                         </div>
-                    </Col>
-                    <Col xs="4" className="ProfileContent">
                         <h2>Name:</h2>
                         <p>{this.state.user.name}</p>
                         <h2>E-Mail:</h2>
                         <p>{this.state.user.email}</p>
                         <h2>City:</h2>
                         <p>{this.state.user.city}</p>
+                    </Col>
+                    <Col xs="2" className="ProfileContent">
+                        <h2>Meine Produkte:</h2>
                         
+                        <ul>
+                        {
+                                this.state.products.map((p) => {
+                                    return <MyProduct setProductID={this.setProductID} key={p._id} product={p} />
+                                })
+                            }
+                        </ul>
                     </Col>
                 </Row>
             </div>
