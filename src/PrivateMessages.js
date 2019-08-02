90f2ec474a02e920ba55ed1ca0e0bedfaf612cfa
NikolaCirisanovic <nikola.cirisanovic@outlook.de>
2019-08-02 16:06:36 +0700

    rendering all prodcuts for each profile in PM section

--- src/PrivateMessages.js
+++ src/PrivateMessages.js
@@ -3,23 +3,76 @@ import {Col, Row} from 'reactstrap'
 import NavMain from './NavMain'
 import NewMessage from './NewMessage'
 import PrivateMessage from './PrivateMessage'
-import ChatSidebar from './ChatSidebar';
+import axios from 'axios'
+import MyProduct from './MyProduct'
 
 class PrivateMessages extends Component {
+
+    state = {
+        user: {},
+        products:[],
+        selectedProduct:''
+    }
+
+    setProductID = (id) => {
+		this.setState({
+			selectedProduct: id
+		}, () => console.log(id))
+    }
+    
+    getProfile = () => {
+		axios.get(`${process.env.REACT_APP_API}/api/profile`, 
+		{headers: {
+			Authorization: `Bearer ${localStorage.getItem('token')}`
+		}}
+		).then((res) => {
+			this.setState({
+                user: res.data
+			}, () => {
+                this.getProductsForMyProfile()
+            })
+		}).catch((err) => {
+			console.log('err', err)
+		})
+    }
+
+    getProductsForMyProfile = () => { 
+        axios.get(`${process.env.REACT_APP_API}/api/products?author=${this.state.user._id}`, 
+        {headers: {
+			Authorization: `Bearer ${localStorage.getItem('token')}`
+		}}
+        ).then((res) => {
+            this.setState({
+                products: res.data
+            }, () => console.log('products', this.state.products))
+        }).catch((err) => {
+            console.log('err', err)
+        })
+    }
+
+    componentWillMount() {
+        this.getProfile()
+    }
+
     render() {
         return (
             <div>
                 <NavMain />
             <Row>
                 <Col sm={2}>
-                <ChatSidebar />
+                <h2>Meine Produkte:</h2>
+                        <ul>
+                             {
+                                this.state.products.map((p) => {
+                                    return <MyProduct setProductID={this.setProductID} key={p._id} product={p} />
+                                })
+                            }
+                         </ul>
                 </Col>
                 
                 <Col sm={9} className="commentSection">
                     <h4>Deine Nachrichten</h4>
                 <div id="messages">
-                    <PrivateMessage/>
-                    <PrivateMessage/>
                     <PrivateMessage/>
                     <PrivateMessage/>
 				</div>
